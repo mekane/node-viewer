246dd92dd040d1c489158b617a615048056ba4b0
Marty Kane <marty.kane@gmail.com>
2019-10-11 22:21:48 -0500

    Improve previous and next links, add some styles

--- index.js
+++ index.js
@@ -7,99 +7,132 @@ console.log(`starting up in ${directory}`);
 const files = fs.readdirSync(directory);
 
 console.log('Buliding list of files');
-files.forEach((f,i) => {
-  console.log(`  ${i}: ${f}`);
+files.forEach((f, i) => {
+    console.log(`  ${i}: ${f}`);
 });
+
+const maxPage = files.length - 1;
 
 startServer();
 
 
 function getDirectory() {
-  const cwd = process.cwd();
-  const arg1 = process.argv[2];
+    const cwd = process.cwd();
+    const arg1 = process.argv[2];
 
-  let dir = cwd;
+    let dir = cwd;
 
-  if (arg1)
-    dir += `/${arg1}`;
+    if (arg1)
+        dir += `/${arg1}`;
 
-  return dir;
+    return dir;
 }
 
 function startServer() {
-  const express = require('express');
-  const port = 6969;
-  const app = express();
-  app.use(express.static(directory));
-  app.get('/', startAtPageZero);
-  app.get('/:p', showPage);
-  app.listen(port, () => console.log(`Server listening on port ${port}!`));
+    const express = require('express');
+    const port = 6969;
+    const app = express();
+    app.use(express.static(directory));
+    app.get('/', startAtPageZero);
+    app.get('/:p', showPage);
+    app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
-  /*
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html'],
-  index: false,
-  maxAge: '1d',
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now())
+    /*
+  var options = {
+    dotfiles: 'ignore',
+    etag: false,
+    extensions: ['htm', 'html'],
+    index: false,
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+      res.set('x-timestamp', Date.now())
+    }
   }
-}
 
-  app.use(express.static('public', options))
-  */
+    app.use(express.static('public', options))
+    */
 }
 
 function startAtPageZero(req, res) {
-  res.redirect(301, '/0');
+    res.redirect(301, '/0');
 }
 
 function showPage(req, res) {
-  console.log(`page requested`, req.params);
-  const p = req.params['p'];
+    console.log(`page requested`, req.params);
+    const p = req.params['p'];
 
-  //validate page
-  const page = parseInt(p, 10);
+    //validate page
+    const page = parseInt(p, 10);
 
-  if ( isNaN(page) || page < 0 || page >= files.length ) {
-    res.status(400);
-    res.send(`Invalid page number ${page}`);
-    return;
-  }
+    if (isNaN(page) || page < 0 || page > maxPage) {
+        res.status(400);
+        res.send(`Invalid page number ${page}`);
+        return;
+    }
 
-  res.send(getPage(page));
+    res.send(getPage(page));
 }
 
 function getPage(p) {
-  return pageHead(p) + pageBody(p) + pageEnd(p);
+    return pageHead(p) + pageBody(p) + pageEnd(p);
 }
 
 function pageHead(page) {
-  return `<!doctype html>
+    return `<!doctype html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Page ${page}</title>
-</head>`
-;
+  <style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  h1 {
+  color: #ccc;
+    margin: 0 auto 2px;
+    text-align: center;
+  }
+
+  a.link {
+    color: #ccc;
+    font-weight: bold;
+    position: absolute;
+    top: 4px;
+  }
+</style>
+</head>`;
 }
 
 function pageBody(page) {
-  const image = files[page];
+    const image = files[page];
 
-  return `<body>
+    let prev = '';
+    let next = '';
+
+    if (page > 0) {
+        prev = `<a class="link" style="left: 4px;" href="/${page-1}">Prev</a>`;
+    }
+
+    if (page < maxPage) {
+        next = `<a class="link" style="right: 4px;" href="/${page+1}">Next</a>`;
+    }
+
+    return `<body style="background: rgb(44, 44, 44)">
+${prev}
   <h1>Page ${page}</h1>
-  <a href="/${image}">
-    <img src="/${image}">
+${next}
+  <a href="/${page < maxPage ? page + 1 : 0}">
+    <img style="display: block; margin: auto;" src="/${image}">
   </a>
 </body>
 `;
 }
 
 function pageEnd(page) {
-  return `</html>`;
+    return `</html>`;
 }
 
